fbb63f04e600922d7d7b1d27d4338b57746b6eec
Shen Yiming <soimy@163.com>
2019-05-28 18:52:55 +0800

    Implemented export class

--- src/exporter.ts
+++ src/exporter.ts
@@ -1,3 +1,36 @@
-import {} from "mustache";
+import Mustache from "mustache";
+import { join } from "path";
+import { existsSync, readFileSync } from "fs";
+import { Bin } from "maxrects-packer";
 
-export class Exporter { }
+let list = require('../templates/list.json');
+
+export class Exporter {
+    static template: string;
+
+    public static setExportFormat (name: string): boolean {
+        name = name.toLowerCase();
+        let templatePath: string = name;
+        for (let t of list) {
+            if (t.type.toLowerCase() === name) {
+                templatePath = join(__dirname, t.template);
+                break;
+            }
+        }
+        if (!existsSync(templatePath)) {
+            console.error(`Spritesheet template [${templatePath}] not found.`);
+            return false;
+        }
+        this.template = readFileSync(templatePath).toString();
+        return true;
+    }
+
+    public static compile (bin: Bin): string {
+        if (!this.template) {
+            // Apply default template if not set by setExportFormat
+            this.template = readFileSync(join(__dirname,'JsonHash')).toString();
+        }
+        return Mustache.render(this.template, bin);
+    }
+
+}
--- src/geom/Sheet.ts
+++ src/geom/Sheet.ts
@@ -5,6 +5,7 @@ import Jimp from "jimp";
 export class Sheet extends Rectangle {
 
     public name: string = "";
+    public frame: Rectangle;
     public sourceFrame: Rectangle;
     public anchor: Vec2;
     public nineSliceFrame: Rectangle;
@@ -19,15 +20,20 @@ export class Sheet extends Rectangle {
         public rot: boolean = false
     ) {
         super();
-        this.sourceFrame = new Rectangle(0, 0, width, height);
+        this.frame = new Rectangle(width, height);
+        this.sourceFrame = new Rectangle(width, height);
         this.anchor = new Vec2(width / 2, height / 2);
-        this.nineSliceFrame = new Rectangle(0, 0, width, height);
+        this.nineSliceFrame = new Rectangle(width, height);
         this.data = new Jimp(width, height);
     }
 
     public trimAlpha (): void {
         // TODO
         this.trimmed = true;
+    }
+
+    public extrude (border: number): void {
+        // TODO
     }
 
     public rotate (): void {
